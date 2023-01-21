bd2cb5447162add9b31827d847c45bfa615e667e
brimdor <chrisnelsonx@gmail.com>
2023-01-21 09:53:57 -0600

    Update apply.sh

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -4,9 +4,9 @@ helm template \
     --dependency-update \
     --include-crds \
     --namespace argocd \
-    argocd . \
-    | kubectl apply -n argocd -f -
+    argocd . |
+    kubectl apply -n argocd -f -
 
-kubectl -n argocd wait --timeout=60s --for condition=Established \
-       crd/applications.argoproj.io \
-       crd/applicationsets.argoproj.io
+kubectl -n argocd wait --timeout=120s --for condition=Established \
+    crd/applications.argoproj.io \
+    crd/applicationsets.argoproj.io
